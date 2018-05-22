e7704763c4584f591aa99959894748b94f852f69
chengzhx76 <chengzhx76@qq.com>
2018-05-22 13:17:05 +0800

    init

---
+++ src/test/java/com/github/chengzhx76/TestRex.java
@@ -0,0 +1,13 @@
+package com.github.chengzhx76;
+
+/**
+ * @desc:
+ * @author: hp
+ * @date: 2018/5/22
+ */
+public class TestRex {
+    public static void main(String[] args) {
+        System.out.println("02 | 微服务的安全要解决什么问题".replaceAll("\\w+", "-"));
+        System.out.println("wei232123jin234".replaceAll("\\d{4,}", "-"));
+    }
+}
